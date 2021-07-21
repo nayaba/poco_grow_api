0f924169f6245868bd15a1626059b66a4fe88799
nayaba <nabila.ayaba@gmail.com>
2021-07-20 22:56:54 -0400

    Fix bug in views

--- prompt/views.py
+++ prompt/views.py
@@ -22,7 +22,7 @@ class Prompts(APIView):
 
     def post(self, request):
         """Create a Prompt"""
-        serializer = PromptSerializer(data=request.data)
+        serializer = PromptSerializer(data=request.data['prompt'])
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
